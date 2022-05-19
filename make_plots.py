3e56d389ee7325054454659848a508a14446dcf6
Nicklas Vedsted <nicklasvedsted@gmail.com>
2022-05-19 13:59:10 +0200

    Plot absolute t-value and make the plot pairs have equal axis dimensions

--- make_plots.py
+++ make_plots.py
@@ -5,6 +5,7 @@ from dask import array
 from dask.array import stats
 from dask.diagnostics import ProgressBar
 from matplotlib import pyplot as plt
+import numpy as np
 
 n = int(sys.argv[1])
 
@@ -17,15 +18,19 @@ unmasked_data2 = array.from_zarr(f'data/{n}_unmasked_random.zarr')
 masked_data1 = array.from_zarr(f'data/{n}_masked_fixed.zarr')
 masked_data2 = array.from_zarr(f'data/{n}_masked_random.zarr')
 
-unmasked_computation = stats.ttest_ind(unmasked_data1, unmasked_data2, equal_var=False)
-masked_computation = stats.ttest_ind(masked_data1, masked_data2, equal_var=False)
+unmasked_computation = np.abs(stats.ttest_ind(unmasked_data1, unmasked_data2, equal_var=False)[0])
+masked_computation = np.abs(stats.ttest_ind(masked_data1, masked_data2, equal_var=False)[0])
 
-(unmasked_statistic, _), (masked_statistic, _) = dask.compute(unmasked_computation, masked_computation)
+unmasked_statistic, masked_statistic = dask.compute(unmasked_computation, masked_computation)
+
+biggest = max(unmasked_statistic.max(), masked_statistic.max())
 
 plt.plot(unmasked_statistic)
+plt.axis([0, len(unmasked_statistic), 0, biggest])
 plt.savefig(f"plots/{n}_unmasked.png")
 
 plt.clf()
 
 plt.plot(masked_statistic)
+plt.axis([0, len(masked_statistic), 0, biggest])
 plt.savefig(f"plots/{n}_masked.png")
